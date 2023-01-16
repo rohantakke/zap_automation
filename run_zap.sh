c8c3e543af7c7ccaa58cc3c9fbe883bbecaec5ad
rohantakke <rohan.takke@accops.com>
2023-01-16 14:51:07 +0530

    minor changes

--- run_zap.sh
+++ run_zap.sh
@@ -1,5 +1,5 @@
 python3 launchSpider.py
 python3 activeScan.py
 rm *.html
-./getReport.sh > $(jq -r .target_url config.json | awk -F// '{print $2}')_$(date +"%d_%m_%y").html
-cp *.html old_reports/
+./getReport.sh > web_application_scanner_report.html
+cp web_application_scanner_report.html old_reports/webapp_$(date +"%d_%m_%y").html
